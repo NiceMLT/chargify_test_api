7b73c0e2c836b95a8066a2d6c38dfa103027ed40
Matt Thompson <matthewlthompson@gmail.com>
2018-04-03 11:42:45 -0400

    fixes specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,22 +1,7 @@
-require 'test_helper'
+require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Person. As you add validations to Person, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # PeopleController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
   describe "POST create" do
     describe "with valid params" do
       it "creates a new User" do
